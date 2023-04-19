d1426c64b64867eccbb19d8fead5b46a6b8dd5b6
elisgugu <130983350+elisgugu@users.noreply.github.com>
2023-04-19 18:30:34 +0300

    Add files via upload
    Added Hubs

---
+++ Hubs/CertificateHub.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.EntityFrameworkCore;
+using notification_system.Models;
+using notification_system.Repository;
+using TableDependency.SqlClient.Base.EventArgs;
+
+namespace notification_system.Hubs {
+
+    public class CertificateHub : Hub {
+        public CertificateHub() {
+           
+        }
+        public async Task SendRequests(List<Certificate> certificates) {
+            await Clients?.All.SendAsync("certificatesExpired", certificates);
+        }
+    }
+}
---
+++ Hubs/RequestHub.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.EntityFrameworkCore;
+using notification_system.Models;
+using notification_system.Repository;
+using TableDependency.SqlClient.Base.EventArgs;
+
+namespace notification_system.Hubs {
+
+    public class RequestHub : Hub {
+        RequestRepository _requestRepository;
+        public RequestHub(IConfiguration configuration, IUserIdProvider userProvider) {
+            _requestRepository = new RequestRepository(configuration);
+        }
+       
+        public async Task SendRequests() {
+            var requests = _requestRepository.GetAllRequests();
+            await Clients.Caller.SendAsync("receivedRequest", requests);
+        }
+
+        internal async void SendUpdatedRecord(RecordChangedEventArgs<Request> e) {
+           if (e.EntityOldValues.StatusId != e.Entity.StatusId) {
+                var requests = _requestRepository.GetAllRequests();
+                var changedRequest = requests.FirstOrDefault(x => x.Id == e.Entity.Id);
+                await Clients.Caller.SendAsync("statusChanged", changedRequest);
+            }
+        }
+    }
+}
