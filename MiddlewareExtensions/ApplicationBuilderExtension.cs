a02eb0f0f44528a660b2e99835b9ea9566d75667
elisgugu <130983350+elisgugu@users.noreply.github.com>
2023-04-19 18:28:05 +0300

    Add files via upload
    Missing Middleware

---
+++ MiddlewareExtensions/ApplicationBuilderExtension.cs
@@ -0,0 +1,12 @@
+﻿using notification_system.SubscribeTableDependencies;
+
+namespace notification_system.MiddlewareExtensions {
+    public static class ApplicationBuilderExtension {
+        public static void UseSqlTableDependency<T>(this IApplicationBuilder applicationBuilder, string connectionString)
+                 where T : ISubscribeTableDependency {
+            var serviceProvider = applicationBuilder.ApplicationServices;
+            var service = serviceProvider.GetService<T>();
+            service.SubscribeTableDependency(connectionString);
+        }
+    }
+}
